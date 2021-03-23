f6ffcf12178bc3762c97f8cdd87675c8c0b9e9d8
Pritam <74299546+Vikas-Pritam@users.noreply.github.com>
2021-03-23 20:49:52 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
-var ball;
+var ball,ball1;
 var database;
 var loac,position;
+var loca,position1;
 
 function setup(){
     createCanvas(500,500);
     database=firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    ball1 = createSprite(250,250,10,10);
+    ball1.shapeColor = "red";
+
+
+
     loac=database.ref('ball/position');
     loac.on("value",readPosition,showError);
+
+    loca=database.ref('ball1/position1');
+    loca.on("value",readPosition1,showError1);
+
+
 
 }
 
@@ -26,6 +38,34 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         writePosition(0,+1);
     }
+
+
+
+
+
+    if(keyDown('a')){
+        writePosition1(-1,0);
+    }
+    else if(keyDown('d')){
+        writePosition1(1,0);
+    }
+    else if(keyDown('w')){
+        writePosition1(0,-1);
+    }
+    else if(keyDown('s')){
+        writePosition1(0,+1);
+    }
+
+
+    if(ball.isTouching(ball1))
+    {
+
+        text("GAME OVER",250,250);
+
+    }
+
+
+
     drawSprites();
 }
 
@@ -44,6 +84,25 @@ function readPosition(data)
 }
  
 function showError()
+{
+
+}
+
+function writePosition1(x,y){
+    database.ref('ball1/position1').set({
+    x :ball1.x + x,
+    y :ball1.y + y
+    })
+}
+
+function readPosition1(data)
+{
+    position1=data.val();
+    ball1.x=position1.x;
+    ball1.y=position1.y;
+}
+ 
+function showError1()
 {
 
 }
